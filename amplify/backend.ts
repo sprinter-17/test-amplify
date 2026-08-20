2321f0cad92c3f3fbb8ca5c99478f7f18e62aa8f
Simon Pincus <simon.pincus@gmail.com>
2026-08-20 20:13:43 +1000

    Added custom lambda

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { hello } from './functions/hello/resource';
 
 defineBackend({
   auth,
   data,
+  hello
 });
